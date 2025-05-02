06705585eebbd2cae22188e97bba9be21cbe0a3d
Adler <isaac.adler@ufrpe.br>
2025-05-02 18:27:50 -0300

    Java
    Código feito em Java

---
+++ src/AnaliseDesempenho/App.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class App {
+
+    //LEIA ANTES DE USAR!!!!
+    //PARA QUE O ARQUIVO SEJA LIDO DO DIRETORIO CERTO DO SEU COMPUTADOR 
+    //USE O EXPLORADOR DE ARQUIVOS PARA LOCALIZAR O ARQUIVO "arq.txt"
+    //DEPOIS CLIQUE COM O BOTÃO DIREITO, VÁ EM PROPRIEDADES E COPIE O >LOCAL<
+    //ENTÃO O ADICIONE NO CAMINHO DO CCÓDIGO EM: File arquivoFile = new File(AQUI O LOCAL DO ARQUIVO)
+    public static void main(String[] args) throws Exception {
+        System.out.println("Executando...");
+        LinkedList linkedList = new LinkedList();
+        File arquivoFile = new File("C:\\Users\\isaac\\Downloads\\Projetos em Java\\ListaLigada\\ListaLigada\\src\\arq-novo.txt");
+        Scanner scanner = null;
+        long inicio = System.nanoTime();
+        //ler o arquivo com scanner apenas no try para fechar na função finally
+
+        try {
+            scanner = new Scanner(arquivoFile);
+            if(scanner.hasNextLine()){
+                String[] valoresIniciais = scanner.nextLine().split(" ");
+                for(String val : valoresIniciais) {
+                    linkedList.Adicionar(Integer.parseInt(val));
+                }        
+            }
+
+            int numeroAcoes = 0;
+            if (scanner.hasNextInt()) {
+                numeroAcoes = scanner.nextInt();
+            }
+
+            int contador = 0;
+            while (scanner.hasNextLine() && contador < numeroAcoes) {
+                String linha = scanner.nextLine().trim();
+                if (linha.isEmpty()) {
+                    continue;
+                }
+
+                String[] partes = linha.split("\\s+");
+                if (partes.length == 0) {
+                    continue;
+                }
+                switch (partes[0]) {
+                    case "A":
+                        int valor = Integer.parseInt(partes[1]);
+                        int posicao = Integer.parseInt(partes[2]);
+
+                        if (posicao < 0 || posicao > linkedList.getTamanho()) {
+                            continue;
+                        }
+
+                        ValorArmazenado novo = new ValorArmazenado(valor);
+
+                        if(posicao == 0) {
+                            novo.setProximo(linkedList.getPrimeiro());
+                            linkedList.setPrimeiro(novo);
+                            if(linkedList.getTamanho() == 0){
+                                linkedList.setUltimo(novo);
+                            }
+                            linkedList.setTamanho(linkedList.getTamanho() + 1);//é bom criar metodo pra incrementar tamanho incrementarTamanho();
+                        }
+
+                        else if(posicao >= linkedList.getTamanho()){
+                                linkedList.Adicionar(valor);
+                        }
+
+                        else {
+                                ValorArmazenado anterior = linkedList.getPosicao(posicao - 1);
+                                novo.setProximo(anterior.getProximo());
+                                anterior.setProximo(novo);
+                                linkedList.setTamanho(linkedList.getTamanho() + 1);
+                        }
+
+                        break;
+                    case "R":
+                        int valorRemover = Integer.parseInt(partes[1]);
+                        boolean removido = linkedList.Remover(valorRemover);
+
+                        if (!removido) {
+                            linkedList.Remover(Integer.parseInt(partes[1]));
+                        }
+                    case "P":
+                        ValorArmazenado atual = linkedList.getPrimeiro();
+                        while (atual != null) {
+                            System.out.print(atual.getValor() + " ");
+                            atual = atual.getProximo();
+                        }
+                        System.out.println();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado: " + e.getMessage());
+
+        }
+        finally {
+            if(scanner != null){
+                scanner.close();
+            }
+            long fim = System.nanoTime();
+            long duracao = fim - inicio; // em nanossegundos
+
+            System.out.println("Tempo de execução: " + duracao / 1_000_000.0 + " ms");
+        }
+    }
+}
---
+++ src/AnaliseDesempenho/LinkedList.java
@@ -0,0 +1,89 @@
+public class LinkedList {
+    private ValorArmazenado primeiro;
+    private ValorArmazenado ultimo;
+    private int tamanho;
+
+    public LinkedList(){
+        this.tamanho = 0;
+    }
+
+    public int getTamanho(){
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho){
+        this.tamanho = tamanho;
+    }
+
+    public ValorArmazenado getPrimeiro() {
+        return primeiro;
+    }
+
+//    public ValorArmazenado getUltimo() {
+//        return ultimo;
+//    }
+
+    public void setPrimeiro(ValorArmazenado primeiro) {
+        this.primeiro = primeiro;
+    }
+
+    public void setUltimo(ValorArmazenado ultimo) {
+        this.ultimo = ultimo;
+    }
+
+    public void Adicionar(int novoValor) {
+        ValorArmazenado novoValorArmazenado = new ValorArmazenado(novoValor);
+        if (primeiro == null && ultimo == null){
+            primeiro = novoValorArmazenado;
+            ultimo = novoValorArmazenado;
+            this.tamanho++;
+        }
+        else{
+            this.ultimo.setProximo(novoValorArmazenado);
+            this.ultimo = novoValorArmazenado;
+            this.tamanho++;
+        }
+    }
+
+    public boolean Remover(int valorParaRemover) {
+        ValorArmazenado atual = this.primeiro;
+        ValorArmazenado anterior = null;
+
+        for (int i = 0; i < this.getTamanho(); i++) {
+            if (atual.getValor() == valorParaRemover) {
+                if (atual == primeiro && atual == ultimo) {
+                    this.primeiro = null;
+                    this.ultimo = null;
+                } else if (atual == primeiro) {
+                    this.primeiro = atual.getProximo();
+                    atual.setProximo(null);
+                } else if (atual == ultimo) {
+                    this.ultimo = anterior;
+                    anterior.setProximo(null);
+                } else {
+                    anterior.setProximo(atual.getProximo());
+                    atual = null;
+                }
+
+                this.tamanho--;
+                return true;
+            }
+
+            anterior = atual;
+            atual = atual.getProximo();
+        }
+
+        return false;
+    }
+
+
+    public ValorArmazenado getPosicao(int posicao){
+        ValorArmazenado atual = this.primeiro;
+        for(int i = 0; i < posicao; i++){
+            if(atual.getProximo() != null){
+                atual = atual.getProximo();
+            }
+        }
+        return atual;
+    }
+}
